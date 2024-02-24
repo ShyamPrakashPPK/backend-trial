d3ca18ec5ec0df0864d0effae5a8b02e0d32df2e
ShyamPrakashPPK <shyamprakash0019@gmail.com>
2024-02-24 10:01:45 +0530

    cors modified

--- server.js
+++ server.js
@@ -29,8 +29,21 @@ const users = [
 
 app.use(express.json());
 
-// Enable CORS for all routes
-app.use(cors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' })); 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        )
+        return res.status(200).json({})
+    }
+    next()
+})
 
 
 // Login endpoint
